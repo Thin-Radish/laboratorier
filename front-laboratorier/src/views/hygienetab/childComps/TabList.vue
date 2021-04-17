d26057505233272394018f2e299884f3550847ba
xzhh <xfhh0302@gmail.com>
2021-04-17 18:28:24 +0800

    提交卫生表组件

---
+++ front-laboratorier/src/views/hygienetab/childComps/TabList.vue
@@ -0,0 +1,54 @@
+<template>
+  <tab-list
+    :listHead="hygienetabs[labId].tabHead"
+    :items="hygiene"
+    :isObj="false"
+    class="tab-list"
+  />
+</template>
+
+<script>
+import TabList from "../../../components/content/List.vue";
+
+import { getHygiene } from 'network/reqData'
+
+export default {
+  components: {
+    TabList,
+  },
+
+  data() {
+    return {
+      hygienetabs: {
+        hygieneF608: {
+          tabHead: ["", "星期一", "星期三", "星期五"],
+          lab_id: "F608",
+        },
+        hygieneF609: {
+          tabHead: ["", "星期一", "星期三", "星期四", "星期五"],
+          lab_id: "F609",
+        },
+      },
+    };
+  },
+  computed: {
+    labId: function () {
+      return this.$route.path.split("/")[2];
+    },
+  },
+  mounted(){
+      getHygiene().then(res=>{
+        // console.log(res);
+        this.hygiene = res
+      })
+    },
+};
+</script>
+
+<style scoped>
+.tab-list {
+  margin-right: 5%;
+  margin: 25px 5% 0 5%;
+  border: 1px solid rgb(217, 236, 255);
+}
+</style>
---
+++ front-laboratorier/src/views/hygienetab/childComps/TabNav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-nav">
+    <nav-content styleNav="row">
+      <nav-item v-for="item in labsId" :key="item" :path="'/HygieneTab/hygiene' + item" >
+        <div slot="nav-text">{{item}}</div>
+      </nav-item>
+    </nav-content>
+  </div>
+</template>
+
+<script>
+import NavContent from "../../../components/common/nav/NavContent";
+import NavItem from "../../../components/common/nav/NavItem";
+
+export default {
+  components: {
+    NavContent,
+    NavItem,
+  },
+  data() {
+    return {
+      labsId: ['F608','F609']
+    }
+  }
+
+};
+</script>
+
+<style scoped>
+
+</style>
